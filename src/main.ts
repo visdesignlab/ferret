a1608cdb16cf8acd626aa928ffeabef5d708ee4c
Devin Lange <devinscottlange@gmail.com>
2021-05-24 14:26:56 -0600

    tweak style on upload only page

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,9 @@ let tableDisplay = new TableDisplay();
 let filterDisplay = new FilterDisplay();
 let highlightDisplay = new HighlightDisplay();
 tableDisplay.SetContainer(tableContainer);
+let outerContainer = document.getElementById('outerContainer');
+let uploadOnlyContainerOuter = document.getElementById('uploadOnlyContainerOuter');
+let uploadOnlyContainerInner = document.getElementById('uploadOnlyContainerInner');
 let toolbarContainer = document.getElementById('toolbar');
 let controlsContainer = document.getElementById('controlsContainer');
 let controlsDisplay = new ControlsDisplay(toolbarContainer, controlsContainer, tableContainer);
@@ -33,19 +36,27 @@ let init = (data: string, filename: string) =>
 	filterDisplay.drawDropdown();
 	highlightDisplay.drawDropdown();
 	document.title = filename;
+	uploadOnlyContainerOuter.classList.add('noDisp');
+	outerContainer.classList.remove('noDisp');
 }
 
 let urlParams = new URLSearchParams(document.location.search)
+let uploadContainer: HTMLElement;
+let bigStyle: boolean;
 if (urlParams.has('data_path'))
 {
 	let filename = urlParams.get('data_path');
 	d3.text(filename).then(data =>
 	{
 		init(data, filename)
-	})
+	});
+	uploadContainer = toolbarContainer;
+	bigStyle = false;
 }
 else
 {
+	uploadContainer = uploadOnlyContainerInner;
+	bigStyle = true;
 }
-let fileLoadButton = new UploadFileButton(toolbarContainer, init);
+let fileLoadButton = new UploadFileButton(uploadContainer, init, bigStyle);
 
