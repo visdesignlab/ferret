5f85615efa1154c0bc4cffc2c29ac03cc3ddbf16
Devin Lange <devinscottlange@gmail.com>
2022-01-26 13:30:08 -0700

    add auto-prefixer

--- gulpfile.js
+++ gulpfile.js
@@ -8,17 +8,31 @@ var paths = {
     pages: ['src/*.html']
 };
 
-var watchedBrowserify = watchify(browserify({
-    basedir: '.',
-    debug: true,
-    entries: ['src/main.ts'],
-    cache: {},
-    packageCache: {}
-}).plugin(tsify));
+var watchedBrowserify = watchify(
+    browserify({
+        basedir: '.',
+        debug: true,
+        entries: ['src/main.ts'],
+        cache: {},
+        packageCache: {}
+    }).plugin(tsify)
+);
 
 gulp.task('copy-html', function () {
-    return gulp.src(paths.pages)
-        .pipe(gulp.dest('dist'));
+    return gulp.src(paths.pages).pipe(gulp.dest('dist'));
+});
+
+gulp.task('autoprefixer', () => {
+    const autoprefixer = require('autoprefixer');
+    const sourcemaps = require('gulp-sourcemaps');
+    const postcss = require('gulp-postcss');
+
+    return gulp
+        .src('./src/css/*.css')
+        .pipe(sourcemaps.init())
+        .pipe(postcss([autoprefixer()]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./dist/css'));
 });
 
 function bundle() {
@@ -29,6 +43,9 @@ function bundle() {
         .pipe(gulp.dest('dist'));
 }
 
-gulp.task('default', gulp.series(gulp.parallel('copy-html'), bundle));
+gulp.task(
+    'default',
+    gulp.series(gulp.parallel('copy-html'), 'autoprefixer', bundle)
+);
 watchedBrowserify.on('update', bundle);
 watchedBrowserify.on('log', fancy_log);
