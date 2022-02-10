visdesignlab/ferret

commit 655e4ab341f6b29ea48e1c80bfc915bcb1e47a80

add highliht/ignore count for n-grams

src/FerretRenderer.ts
@@ -417,12 +417,23 @@ export default class FerretRenderer implements ICellRendererFactory {
         n: number,
         lsd: boolean
     ): Promise<void> {
-        let nGramFrequency = await ChartCalculations.GetNGramFrequency(
-            column,
-            context,
-            n,
-            lsd
-        );
+        // let nGramFrequency = await ChartCalculations.GetNGramFrequency(
+        //     column,
+        //     context.provider,
+        //     n,
+        //     lsd
+        // );
+        const elementID = chartKey + colKey;
+        container.id = elementID;
+        const vizContainer = container.querySelector('.innerVizContainer');
+        vizContainer.id = elementID + '-inner';
+
+        let nGramFrequency = column?.ngramCounts?.acknowledged ?? [];
+        if (nGramFrequency.length === 0) {
+            vizContainer.innerHTML = '...';
+            return;
+        }
+
         let dataValues: Array<any> = [];
         let index = 0;
         let multiFrequentGrams: Array<any> = [];
@@ -447,10 +458,6 @@ export default class FerretRenderer implements ICellRendererFactory {
                 count: count
             });
         }
-
-        const elementID = chartKey + colKey;
-        container.id = elementID;
-        container.querySelector('.innerVizContainer').id = elementID + '-inner';
 
         var yourVlSpec: VisualizationSpec = {
             width: 85,

src/TableDisplay.ts
@@ -10,7 +10,8 @@ import clog from './lib/clog';
 import {
     ChartCalculations,
     LeadDigitCountMetadata,
-    FreqValsMetadata
+    FreqValsMetadata,
+    NGramMetadata
 } from './ChartCalculations';
 
 export class TableDisplay extends EventTarget {
@@ -176,12 +177,31 @@ export class TableDisplay extends EventTarget {
         clog.h1('updateFerretColumnMetaData');
 
         const freqValPromises: Promise<FreqValsMetadata>[] = [];
-        // const leadingDigitPromises: Promise<LeadDigitCountMetadata>[] = [];
+        const nGramPromises: Promise<NGramMetadata>[] = [];
         const leadingDigitPromises: Promise<LeadDigitCountMetadata>[] = [];
         for (let col of this.ferretColumns) {
+            // Frequent Values
             freqValPromises.push(
                 ChartCalculations.GetDuplicateCounts(col, this.lineup.data)
             );
+            // N-Grams
+            const twoGramSwitch = document.getElementById(
+                '2-gram-switch'
+            ) as HTMLInputElement;
+            const nGram = twoGramSwitch.checked ? 2 : 3;
+            const lsdSwitch = document.getElementById(
+                'lsd-switch'
+            ) as HTMLInputElement;
+            const lsd = lsdSwitch.checked;
+            nGramPromises.push(
+                ChartCalculations.GetNGramFrequency(
+                    col,
+                    this.lineup.data,
+                    nGram,
+                    lsd
+                )
+            );
+            // Leading Digit Frequency
             leadingDigitPromises.push(
                 ChartCalculations.getLeadingDigitCounts(col, this.lineup.data)
             );
@@ -192,6 +212,13 @@ export class TableDisplay extends EventTarget {
             let col = this.ferretColumns[i];
             let freqVals = freqValsList[i];
             col.freqVals = freqVals;
+        }
+        // N-Grams
+        let nGramsList = await Promise.all(nGramPromises);
+        for (let i = 0; i < this.ferretColumns.length; i++) {
+            let col = this.ferretColumns[i];
+            let nGramCounts = nGramsList[i];
+            col.ngramCounts = nGramCounts;
         }
         // Leading Digit Frequency
         let digitCountsList = await Promise.all(leadingDigitPromises);

src/ChartCalculations.ts
@@ -7,6 +7,7 @@ export interface SelectionMetadata<T> {
 }
 
 export type FreqValsMetadata = SelectionMetadata<[number, number][]>;
+export type NGramMetadata = SelectionMetadata<[string, number][]>;
 export type LeadDigitCountMetadata = SelectionMetadata<Map<number, number>>;
 
 export class ChartCalculations {
@@ -197,18 +198,17 @@ export class ChartCalculations {
 
     public static async GetNGramFrequency(
         column: FerretColumn,
-        context: IRenderContext,
+        provider: IDataProvider,
         n: number,
         lsd: boolean
-    ): Promise<[string, number][]> {
-        let nGramFrequencyMap: Map<string, number> = new Map<string, number>();
+    ): Promise<NGramMetadata> {
+        let ignoredFrequencyMap = new Map<string, number>();
+        let acknowledgedFrequencyMap = new Map<string, number>();
+
         const ranking = column.findMyRanker();
         const indices = ranking.getOrder();
         for (let i of indices) {
-            const dataRow = await context.provider.getRow(i);
-            if (column.ignoreInAnalysis(dataRow)) {
-                continue;
-            }
+            const dataRow = await provider.getRow(i);
             const val = column.getRaw(dataRow);
 
             let valString = val.toString();
@@ -218,6 +218,10 @@ export class ChartCalculations {
             valString = lsd
                 ? valString.substr(valString.indexOf('.'))
                 : valString;
+
+            let nGramFrequencyMap = column.ignoreInAnalysis(dataRow)
+                ? ignoredFrequencyMap
+                : acknowledgedFrequencyMap;
             for (let i = 0; i < valString.length; i++) {
                 let currentCount = 0;
                 let nGram = valString.substr(i, n);
@@ -237,17 +241,20 @@ export class ChartCalculations {
             }
         }
 
-        let nGramFrequency = Array.from(nGramFrequencyMap);
+        let ignored = Array.from(ignoredFrequencyMap);
+        let acknowledged = Array.from(acknowledgedFrequencyMap);
 
-        nGramFrequency.sort((a: [string, number], b: [string, number]) => {
-            if (a[1] > b[1]) {
-                return -1;
-            } else if (a[1] < b[1]) {
-                return 1;
-            }
-            return 0;
-        });
+        for (let nGramFrequency of [ignored, acknowledged]) {
+            nGramFrequency.sort((a: [string, number], b: [string, number]) => {
+                if (a[1] > b[1]) {
+                    return -1;
+                } else if (a[1] < b[1]) {
+                    return 1;
+                }
+                return 0;
+            });
+        }
 
-        return nGramFrequency;
+        return { ignored, acknowledged };
     }
 }

src/DropdownBase.ts
@@ -217,6 +217,7 @@ export abstract class DropdownBase extends EventTarget {
     ): number {
         let count: number;
         let num_val = +val;
+        let num_str = val.toString();
         type ValType =
             | 'value.acknowledged'
             | 'value.ignored'
@@ -260,8 +261,36 @@ export abstract class DropdownBase extends EventTarget {
                 }
                 break;
             case 'nGram.acknowledged':
+                if (col) {
+                    count =
+                        col.ngramCounts.acknowledged.find(
+                            ([val, _count]) => val === num_str
+                        )?.[1] ?? 0;
+                } else {
+                    count = d3.sum(
+                        allColumns,
+                        d =>
+                            d?.ngramCounts.acknowledged.find(
+                                ([val, _count]) => val === num_str
+                            )?.[1] ?? 0
+                    );
+                }
                 break;
             case 'nGram.ignored':
+                if (col) {
+                    count =
+                        col.ngramCounts.ignored.find(
+                            ([val, _count]) => val === num_str
+                        )?.[1] ?? 0;
+                } else {
+                    count = d3.sum(
+                        allColumns,
+                        d =>
+                            d?.ngramCounts.ignored.find(
+                                ([val, _count]) => val === num_str
+                            )?.[1] ?? 0
+                    );
+                }
                 break;
             case 'leadingDigit.acknowledged':
                 count =

src/FerretColumn.ts
@@ -1,15 +1,10 @@
-import {
-    Column,
-    IDataRow,
-    ValueColumn,
-    ECompareValueType,
-    IRenderContext
-} from 'lineupjs';
+import { Column, IDataRow, ValueColumn, ECompareValueType } from 'lineupjs';
 import { IEventListener } from 'lineupjs/build/src/internal';
 import {
     ChartCalculations,
     FreqValsMetadata,
-    LeadDigitCountMetadata
+    LeadDigitCountMetadata,
+    NGramMetadata
 } from './ChartCalculations';
 
 export interface FerretSelection {
@@ -71,6 +66,14 @@ export default class FerretColumn extends ValueColumn<number> {
     }
     public set freqVals(v: FreqValsMetadata) {
         this._freqVals = v;
+    }
+
+    private _ngramCounts: NGramMetadata;
+    public get ngramCounts(): NGramMetadata {
+        return this._ngramCounts;
+    }
+    public set ngramCounts(v: NGramMetadata) {
+        this._ngramCounts = v;
     }
 
     private static _globalIgnore: FerretSelection = {

commit bd5f8a4561cb3b5acdd5c6d639dc1890f7509978

fix 3-gram and lsd for n-grams (broken in commit 655e4ab341f6b29ea48e1c80bfc915bcb1e47a80)

src/FerretRenderer.ts
@@ -200,10 +200,6 @@ export default class FerretRenderer implements ICellRendererFactory {
         vizContainer.id = elementID + '-inner';
 
         let dupCounts = column?.freqVals?.acknowledged ?? [];
-        if (dupCounts.length === 0) {
-            vizContainer.innerHTML = '...';
-            return;
-        }
 
         let selectionName = filterNames.FREQUENT_VALUES_SELECTION;
         let dataValues: Array<any> = [];
@@ -411,10 +407,6 @@ export default class FerretRenderer implements ICellRendererFactory {
         vizContainer.id = elementID + '-inner';
 
         let nGramFrequency = column?.ngramCounts?.acknowledged ?? [];
-        if (nGramFrequency.length === 0) {
-            vizContainer.innerHTML = '...';
-            return;
-        }
 
         let dataValues: Array<any> = [];
         let index = 0;
@@ -521,10 +513,6 @@ export default class FerretRenderer implements ICellRendererFactory {
         clog.h1('drawLeadingDigitDist');
         let digitCounts: Map<number, number>;
         digitCounts = new Map(column?.leadingDigitCounts?.acknowledged);
-        if (digitCounts.size === 0) {
-            container.innerHTML = '...';
-            return;
-        }
         const leadDictFreq = await ChartCalculations.GetLeadingDigitFreqs(
             column,
             context.provider,

src/TableDisplay.ts
@@ -31,7 +31,8 @@ export class TableDisplay extends EventTarget {
     ];
     constructor() {
         super();
-        document.addEventListener('updateLineup', (e: CustomEvent) => {
+        document.addEventListener('updateLineup', async (e: CustomEvent) => {
+            await this.updateFerretColumnMetaData();
             this.lineup.update();
         });
         document.addEventListener('highlightRows', (e: CustomEvent) => {